43ec3a2b541a157ed51a3245fe370199cb9fdd98
yujin19 <35741231+yujin19@users.noreply.github.com>
2019-07-13 10:30:11 -0700

    206

---
+++ leetcode-206.js
@@ -0,0 +1,30 @@
+// leetcode 206
+// language: js
+// https://leetcode.com/problems/reverse-linked-list/
+// author: yujin19
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if (head === null || head.next === null) {
+    return head;
+  }
+  var pre = null;
+  var cur = head;
+  var next = null;
+  while (cur !== null) {
+    next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = next;
+  }
+  return pre;
+};
