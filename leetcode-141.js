c7c63f5cddb78eb1e7094c0ef06f31daa15270f4
yujin19 <35741231+yujin19@users.noreply.github.com>
2019-07-14 16:35:54 -0700

    141

---
+++ leetcode-141.js
@@ -0,0 +1,31 @@
+// leetcode 141
+// language: js
+// https://leetcode.com/problems/linked-list-cycle/
+// author: yujin19
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+  if (head == null || head.next == null) {
+    return false;
+  }
+  let slow = head;
+  let fast = head;
+  while (fast.next !== null && fast.next.next !== null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) {
+      return true;
+    }
+  }
+  return false;
+};
